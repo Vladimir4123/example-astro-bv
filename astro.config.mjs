ee1a56251554bd4a38b8fa3d101bacf6e0817b86
Vladimir4123 <vladimirbn24@gmail.com>
2026-04-24 14:54:58 +0900

    4 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://vladimir4123.github.io',
+  	base: '/example-astro-bv',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
